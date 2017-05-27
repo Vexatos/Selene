0e7cf3925b9ca4170126ea1d64d51d983dd58be8
Vexatos <Stuarzt@gmx.de>
2017-05-27 21:01:18 +0200

    Fixed /bin/selene.lua not working without liveMode.

--- selene/wrappers/OpenOS/bin/selene.lua
+++ selene/wrappers/OpenOS/bin/selene.lua
@@ -4,19 +4,22 @@ if _G._selene and _G._selene.liveMode then
 else
   local env = setmetatable({}, {__index = _ENV})
   local parser = require("selene.parser")
+  local selene = require("selene")
+  selene.load(env)
   env._PROMPT = "selene> "
   local oldload = env.load
-  env.load = function(ld, src, mv, env) 
-    local s = ""
+  env.load = function(ld, src, mv, loadenv)
     if type(ld) == "function" then
+      local s = ""
       local nws = ld()
       while nws and #nws > 0 do
         s = s .. nws
         nws = ld()
       end
+      ld = s
     end
     ld = parser.parse(ld)
-    return oldload(ld, src, mv, env)
+    return oldload(ld, src, mv, loadenv)
   end
   shell.execute("lua", env)
 end
